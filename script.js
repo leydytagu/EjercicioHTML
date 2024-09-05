82411ea1c4df92f30d34c3549f1d0662dd59f2f0
leydytagu <leydy.tarazona@gmail.com>
2024-09-05 16:13:14 -0500

    Subiendo primer intento

---
+++ script.js
@@ -0,0 +1,6 @@
+// Seleccionar el botón y agregar un evento de clic
+const boton = document.getElementById('miBoton');
+
+boton.addEventListener('click', function() {
+    alert('¡Haz hecho clic en el botón!');
+});
